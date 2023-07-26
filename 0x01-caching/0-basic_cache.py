3fa441b758ac19315ad80457c3ba03be7bd37943
Mdlalose4life <blessing1059@gmail.com>
2023-07-26 04:28:50 +0200

    Basic dictionary

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -16,7 +16,8 @@ class BasicCache(BaseCaching):
         """
         if key is None or item is None:
             return
-        else self.cache_data[key] = item
+        else:
+            self.cache_data[key] = item
 
 
     def get(self, key):
