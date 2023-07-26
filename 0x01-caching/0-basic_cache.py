f3245fb6e9ebe0bd450f24c376900c7ea2eeb77e
Mdlalose4life <blessing1059@gmail.com>
2023-07-26 04:31:37 +0200

    keys

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -23,6 +23,6 @@ class BasicCache(BaseCaching):
     def get(self, key):
         """
         """
-        if key is None or key not in self.cache_data:
+        if key is None or key not in self.cache_data.keys():
             return None
         return self.cache_data.get(key)
