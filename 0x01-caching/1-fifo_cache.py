e00239e8113f5a5c8eb3a345b997cc354e159e17
Mdlalose4life <blessing1059@gmail.com>
2023-07-26 17:56:18 +0200

    FIFO

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -34,7 +34,7 @@ class FIFOCache(BaseCaching):
             pass
         else:
             # Length of the catch must not exceed the limits
-            if len(self.cache_data) >= BaseCaching.MAX_ITEMS\
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS \
                     and key not in self.cache_data.keys():
                 # Find the first element on the catch
                 oldest_key = next(iter(self.cache_data.keys()))
