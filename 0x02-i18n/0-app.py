5667081e51317ee78c062ad7d07f139440d899ac
Mdlalose4life <blessing1059@gmail.com>
2023-08-02 03:46:19 +0200

    0-app

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -6,7 +6,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
+def hello_world():
     """Hello
     """
     return render_template('0-index.html')
