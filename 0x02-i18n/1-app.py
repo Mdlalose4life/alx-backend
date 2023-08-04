d8d84efe55a611d1ef8bde162716ef6e6bc3dc9b
Mdlalose4life <blessing1059@gmail.com>
2023-08-05 01:47:02 +0200

    Spaces

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -7,18 +7,21 @@ from flask_babel import Babel
 app = Flask(__name__)
 babel = Babel(app)
 
+
 class Config(object):
-    """instantiating the Babel object in the app. 
+    """instantiating the Babel object in the app.
     """
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
+
 
 @app.route('/')
 def index():
     """timezones
     """
     return render_template('1-index.html')
+
 
 if __name__ == '__main__':
     app.run(port="5000", host="0.0.0.0", debug=True)
