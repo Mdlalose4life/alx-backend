58c51ada48bb5ebaf194682fbd2f80905ad3dfc6
Mdlalose4life <blessing1059@gmail.com>
2023-08-30 02:54:56 +0200

    Sending the Notifications

---
+++ 0x03-queuing_system_in_js/6-job_processor.js
@@ -0,0 +1,15 @@
+#!/usr/bin/yarn dev
+import { Job, createQueu } from 'kue'
+
+const queue = createQueu();
+
+const sendNotification = (phoneNumber, message) => {
+  console.log(`Sending notification to ${ phoneNumber },`, 
+  `with message: ${ message }`
+  );
+};
+
+queue.process('push_notification_code', (job, done) => {
+    sendNotification(job.phoneNumber, job.message);
+    done();
+});
